0aae22c698fd7299105c36e5fade956ddbd15775
Major.Painage <breckdemers@hotmail.com>
2013-05-23 17:56:30 -0400

    Initial Files

---
+++ @START_SERVER.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Stopping game server...
+taskkill /F /IM arma2oaserver.exe
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo Cleanup and respawn vehicles in the Origins DB hive...
+cd /D "C:\YOUR\MYSQL\FOLDER\LOCATION"
+mysql.exe --user=dayz --password=<PASSWORD> --execute="call pCleanup()" dayz_origins
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo Updating Origins banlist...
+cd /D "C:\YOUR\GAMESERVER\FOLDER\LOCATION"
+perl update_bans.pl --save "C:\YOUR\GAMESERVER\FOLDER\LOCATION\dayz_1.origins.tavi\BattlEye\bans.txt" --banz --cbl --dwarden --mybans "C:\YOUR\GAMESERVER\FOLDER\LOCATION\mybanz.txt" --verbose
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo Starting server...
+start .\Expansion\beta\arma2oaserver.exe -ip=<SERVER IP> -beta=Expansion\beta;Expansion\beta\Expansion -nosplash -cpuCount=6 -name=Origins -profiles=dayz_1.origins.tavi -cfg=dayz_1.origins.tavi\basic.cfg -config=dayz_1.origins.tavi\config.cfg -mod=expansion;expansion\beta;expansion\beta\expansion;@DayzOrigins;@dayz_1.origins.tavi
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo Leaving the launcher...
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+exit
